dda1841c35363e53d4a5fe683b079e1720aebb5b
kaipi10hou <kaipi10hou@gmail.com>
2024-08-30 20:53:04 +0900

    ãSSR A도서 조회 선실행

---
+++ src/lib/fetch-books.ts
@@ -0,0 +1,14 @@
+import {BookData} from "@/styles/types";
+
+export default async function fetchBooks(): Promise<BookData[]> {
+    const url = "http://localhost:12345/book"
+
+    try {
+        const response = await fetch(url)
+        return await response.json()
+    } catch(err) {
+        console.error(err)
+        return []
+    }
+
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,43 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import {InferGetServerSidePropsType} from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
-  return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
-  );
+export const getServerSideProps = async() => {
+    const allBooks = await fetchBooks()
+    const recoBooks = await fetchRandomBooks()
+
+    return {
+        props: {
+            allBooks,
+            recoBooks,
+        }
+    }
+}
+
+export default function Home({allBooks, recoBooks}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    return (
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                {recoBooks.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                {allBooks.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </section>
+        </div>
+    );
 }
 
 Home.getLayout = (page: ReactNode) => {
-  return <SearchableLayout>{page}</SearchableLayout>;
+    return <SearchableLayout>{page}</SearchableLayout>;
 };
