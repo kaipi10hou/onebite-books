a302c77fd404d39aeadd1f4fbbb54afe4e0378c7
kaipi10hou <kaipi10hou@gmail.com>
2024-09-07 16:34:13 +0900

    vercel deploy

--- src/lib/fetch-one-book.ts
+++ src/lib/fetch-one-book.ts
@@ -1,7 +1,7 @@
 import {BookData} from "@/styles/types";
 
 export default async function fetchOneBook(id: number): Promise<BookData | null> {
-    const url = `http://localhost:12345/book/${id}`
+    const url = `https://onebite-books-server-pink.vercel.app/book/${id}`
     try {
         const response = await fetch(url)
         if(!response.ok)
--- src/lib/fetch-random-books.ts
+++ src/lib/fetch-random-books.ts
@@ -1,7 +1,7 @@
 import {BookData} from "@/styles/types";
 
 export default async function fetchRandomBooks(): Promise<BookData[]> {
-    const url = "http://localhost:12345/book/random"
+    const url = "https://onebite-books-server-pink.vercel.app/book/random"
 
     try {
         const response = await fetch(url);
